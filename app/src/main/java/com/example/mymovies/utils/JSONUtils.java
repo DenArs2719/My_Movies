f5e77ed5ea456b368feef653b82c1c7718378de8
Dzianis Yauseyenka <dzianis.yauseyenka@unilodz.eu>
2020-10-01 10:40:23 +0200

    Created 2 classes, who will help us to work with network and JSON

---
+++ app/src/main/java/com/example/mymovies/utils/JSONUtils.java
@@ -0,0 +1,5 @@
+///convert JSON in object
+package com.example.mymovies.utils;
+
+public class JSONUtils {
+}
---
+++ app/src/main/java/com/example/mymovies/utils/NetworkUtils.java
@@ -0,0 +1,5 @@
+///class who will help us to work with network(take films from www)
+package com.example.mymovies.utils;
+
+public class NetworkUtils {
+}
