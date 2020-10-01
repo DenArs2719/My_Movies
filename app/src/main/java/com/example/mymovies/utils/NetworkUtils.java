51404f5cc952f52fe803960e678519f0d40bf390
Dzianis Yauseyenka <dzianis.yauseyenka@unilodz.eu>
2020-10-01 10:53:30 +0200

    Created fields for data

--- app/src/main/java/com/example/mymovies/utils/NetworkUtils.java
+++ app/src/main/java/com/example/mymovies/utils/NetworkUtils.java
@@ -1,5 +1,12 @@
 ///class who will help us to work with network(take films from www)
 package com.example.mymovies.utils;
 
-public class NetworkUtils {
+public class NetworkUtils
+{
+    private static final String API_KEY = "0936d8c770393bd195267285dea65d1d";
+    private static final String BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+    private static final String PARAMS_API_KEY = "api_key";
+    private static final String PARAMS_LANGUAGE = "language";
+    private static final String PARAMS_SORT_BY = "sort_by";
+    private static final String PARAMS_PAGE = "page";
 }
