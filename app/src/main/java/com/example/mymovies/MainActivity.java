bfee73c7d24d59fba5a35476aa24cfe0b1551cd3
Dzianis Yauseyenka <dzianis.yauseyenka@unilodz.eu>
2020-10-11 14:44:27 +0200

    Added switch

--- app/src/main/java/com/example/mymovies/MainActivity.java
+++ app/src/main/java/com/example/mymovies/MainActivity.java
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
 import com.example.mymovies.data.Movie;
@@ -21,6 +25,9 @@ public class MainActivity extends AppCompatActivity
 {
     private RecyclerView recyclerViewPosters;
     private MovieAdapter adapter;
+    private Switch switchSort;
+    private TextView textViewPopularity;
+    private TextView textViewTopRated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,18 +36,65 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         recyclerViewPosters = findViewById(R.id.recyclerViewPosters);
         recyclerViewPosters.setLayoutManager(new GridLayoutManager(this,2));
+        switchSort = findViewById(R.id.switchSort);
+        textViewPopularity = findViewById(R.id.textViewPopularity);
+        textViewPopularity = findViewById(R.id.textViewTopRated);
         adapter = new MovieAdapter();
+        switchSort.setChecked(true);
+        ///устанавливаем адаптер в recyclerViewPosters
+        recyclerViewPosters.setAdapter(adapter);
+
+        ///устанавливаем слушатель
+        switchSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked)
+            {
+                setMethodOfSort(isChecked);
+            }
+        });
+        switchSort.setChecked(false);
+
+
+    }
+
+    public void onClickSetPopularuty(View view)
+    {
+        setMethodOfSort(false);
+        switchSort.setChecked(false);
+    }
+
+
+    public void onClickSetTopRated(View view)
+    {
+        setMethodOfSort(true);
+        switchSort.setChecked(true);
+    }
+
+    private void setMethodOfSort(boolean isTopRated)
+    {
+        int methodOfSort = 0;
+
+        if(isTopRated)
+        {
+            methodOfSort = NetworkUtils.TOP_RATED;
+            textViewTopRated.setTextColor(getResources().getColor(R.color.colorAccent));
+            textViewPopularity.setTextColor(getResources().getColor(R.color.white_color));
+        }
+        else
+        {
+            methodOfSort = NetworkUtils.POPULARITY;
+            textViewPopularity.setTextColor(getResources().getColor(R.color.colorAccent));
+            textViewTopRated.setTextColor(getResources().getColor(R.color.white_color));
+        }
+
         ///получем список фильмов
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY,2);
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort,1);
 
         ///получем список фильмом
         ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
-
         ///устанавливаем фильмы у адаптера
         adapter.setMovies(movies);
-
-        ///устанавливаем адаптер в recyclerViewPosters
-        recyclerViewPosters.setAdapter(adapter);
 
     }
 }
